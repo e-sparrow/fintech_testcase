3434c63bb48154439ad0835d9846fb885cd20207
Alexander Voronin <alexander.voronin@gmail.com>
2025-04-02 11:56:30 +0300

    Initial version

---
+++ TaskManager/Pages/Users/Index.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TaskManager.Models;
+using TaskManager.Services;
+
+namespace TaskManager.Pages.Users;
+
+public class IndexModel : PageModel
+{
+    private readonly IDataService _dataService;
+    public List<User> Users { get; set; } = new();
+
+    public IndexModel(IDataService dataService)
+    {
+        _dataService = dataService;
+    }
+
+    public async Task OnGetAsync()
+    {
+        Users = await _dataService.GetUsersAsync();
+    }
+}
